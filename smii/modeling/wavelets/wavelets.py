2c645a6bb8715c0a2f01112868c544744147a45a
Alan Richardson <arichar@tcd.ie>
2018-06-16 12:50:38 +0100

    changed ricker arange so arange is up to int, then scaled and shifted

--- smii/modeling/wavelets/wavelets.py
+++ smii/modeling/wavelets/wavelets.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def ricker(freq, length, dt, peak_time):
     """Return a Ricker wavelet with the specified central frequency."""
-    t = np.arange(-peak_time, length * dt - peak_time, dt, dtype=np.float32)
+    t = np.arange(length, dtype=np.float32) * dt - peak_time
     y = (1 - 2 * np.pi**2 * freq**2 * t**2) \
             * np.exp(-np.pi**2 * freq**2 * t**2)
     return y
